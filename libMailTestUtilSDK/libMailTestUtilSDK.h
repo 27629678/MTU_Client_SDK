aa4c16141c225d78ed09996dd5ce3b0ef25d5e11
yuxiaohua <hzyuxiaohua@corp.netease.com>
2015-03-17 13:34:23 +0800

    Initial Commit

---
+++ libMailTestUtilSDK/libMailTestUtilSDK.h
@@ -0,0 +1,13 @@
+//
+//  libMailTestUtilSDK.h
+//  libMailTestUtilSDK
+//
+//  Created by H-YXH on 3/17/15.
+//  Copyright (c) 2015 migo Co., Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libMailTestUtilSDK : NSObject
+
+@end
---
+++ libMailTestUtilSDK/libMailTestUtilSDK.m
@@ -0,0 +1,13 @@
+//
+//  libMailTestUtilSDK.m
+//  libMailTestUtilSDK
+//
+//  Created by H-YXH on 3/17/15.
+//  Copyright (c) 2015 migo Co., Ltd. All rights reserved.
+//
+
+#import "libMailTestUtilSDK.h"
+
+@implementation libMailTestUtilSDK
+
+@end
