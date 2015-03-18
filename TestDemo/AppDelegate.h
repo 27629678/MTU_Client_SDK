8c6c5a20a7cf37e565a48ce8694f0e7130c4305a
yuxiaohua <hzyuxiaohua@corp.netease.com>
2015-03-18 09:44:01 +0800

    add new target:testdemo

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by H-YXH on 3/18/15.
+//  Copyright (c) 2015 migo Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by H-YXH on 3/18/15.
+//  Copyright (c) 2015 migo Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
