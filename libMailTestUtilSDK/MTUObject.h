164aaa9f49c335d0a9136d2292b9097642c00bd4
yuxiaohua <hzyuxiaohua@corp.netease.com>
2015-03-18 09:29:03 +0800

    add extend base classes

--- libMailTestUtilSDK/MTUObject.h
+++ libMailTestUtilSDK/MTUObject.h
@@ -9,10 +9,10 @@
 #import <Foundation/Foundation.h>
 
 typedef NS_ENUM(NSUInteger, MTUStatusCode) {
-    MTUStatusCode_Success,                          // 成功
-    MTUStatusCode_UserCancel,                       // 取消
-    MTUStatusCode_RequestFail,                      // 请求失败
-    MTUStatusCode_Unsupport,                        // 请求/响应不支持
+    MTUStatusCode_Success = 0,                      // 成功
+    MTUStatusCode_UserCancel = -1,                  // 取消
+    MTUStatusCode_RequestFail = -2,                 // 请求失败
+    MTUStatusCode_Unsupport = -3,                   // 请求/响应不支持
 };
 
 // 请求基类
@@ -30,5 +30,20 @@ typedef NS_ENUM(NSUInteger, MTUStatusCode) {
 @property (nonatomic, retain) NSString* errStr;     // 响应描述
 
 @property (nonatomic, assign) int type;             // 响应类型
+
+@end
+
+// 请求当前配置项
+@interface MTUCurrentConfiReq : MTUBaseReq
+
+@end
+
+// 设定指定配置项
+@interface MTUSetConfigReq : MTUBaseReq
+
+@end
+
+// 设定指定功能开启与关闭
+@interface MTUEnableFunctionReq : MTUBaseReq
 
 @end
